f2f6569736fc9bf5207bb2abc82ab899a6e9755f
nurzod <nurzod.ruzikulov@gmail.com>
2020-05-03 14:25:14 +0500

    Diplom ishi

---
+++ src/main/java/com/manbalar/model/Category.java
@@ -0,0 +1,13 @@
+package com.manbalar.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "category")
+public class Category implements Serializable {
+
+
+}
