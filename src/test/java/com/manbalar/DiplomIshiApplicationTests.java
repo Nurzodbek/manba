a19659e7205ca19a74a148a4c6213deb2334ae81
nurzod <nurzod.ruzikulov@gmail.com>
2020-05-03 13:02:31 +0500

    Diplom ishi

---
+++ src/test/java/com/manbalar/DiplomIshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manbalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomIshiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
