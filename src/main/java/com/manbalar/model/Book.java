b2b8722499df58fa21038c8e507b390f1e5475a0
nurzod <nurzod.ruzikulov@gmail.com>
2020-05-03 14:23:44 +0500

    Diplom ishi

---
+++ src/main/java/com/manbalar/model/Book.java
@@ -0,0 +1,13 @@
+package com.manbalar.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "book")
+public class Book implements Serializable {
+
+
+}
